fca1390b27a9c01c7b5150cde3176b1cd0694272
TEJAS <tejasgowda0524@gmail.com>
2024-03-04 23:36:43 +0530

    ,,

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace exception
+{
+
+    class demo : Exception
+    {
+        public void devidebyzero()
+        {
+            Console.WriteLine("exception has occured!");
+        }
+    }
+    class operation 
+    {
+        public double devisionOperation(double a, double b)
+        {
+            if (b==0)
+            {
+                demo d = new demo();    
+                d.devidebyzero();
+            }
+               
+            return a/b;
+        }
+    }
+
+
+    public  class Program 
+    {
+        static void Main(string[] args)
+        {
+            operation op = new operation();
+            double num = 5, den = 0, quotient;
+
+            quotient = op.devisionOperation(num, den);
+            Console.WriteLine(quotient);
+
+
+
+        }
+    }
+}
